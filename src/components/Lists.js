e08a620a9c0bd40c3360eec33f040840bc5493b4
Almantas1112 <aalmantass1112@gmail.com>
2022-05-09 14:31:57 +0300

    Adding (nothing here or not found) function

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -2,10 +2,21 @@ import React from 'react'
 import ListItem from './ListItem'
 
 const Lists = ({done, del, todos}) => {
+
+    if (todos.length <= 0) {
+        return (
+            <div className='jumbotron text-center'>
+                <h1 className='text-danger'>Nothing added yet!</h1>
+                <h4>Add something!</h4>
+            </div>
+        )
+    }
+
   return (
     <div className='listWrap'>
         <ul className='list-group'>
-            {todos.map((todo) => (
+            {todos && 
+            todos.map((todo) => (
             <ListItem 
                 id={todo.id}
                 key={todo.id} 
