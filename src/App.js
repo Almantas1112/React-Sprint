273781e0fd455e8ac4a109de4266aa80ff9bf6f9
Almantas1112 <aalmantass1112@gmail.com>
2022-05-18 10:45:48 +0300

    Fixing BrowserRouter to HashRouter (error 404 bug fix)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import ShoppingList from "./components/shoppingListApp/ShoppingList";
 import Counter from "./components/counter/CounterApp";
 import Header from "./components/homePage/Header";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
+import NotFound from "./components/ThisIsFine.gif"
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Header />
     <Routes>
       <Route path='/shoppingListApp' element={<ShoppingList />} />
       <Route path='/counter' element={<Counter />} />
       <Route index element={<ShoppingList />} />
-      <Route path="*" element={<ShoppingList />} />
+      <Route path="/*" element={                        
+                          <main style={{ marginTop: "5rem", textAlign: "center", color: "white"}}>
+                          <h1>ERROR 404! Page not found!</h1>
+                          <img style={{maxWidth: "320px"}} src={NotFound} alt="Not found" />
+                        </main>
+                      }
+                       />
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
